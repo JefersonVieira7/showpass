d98ce6a5f223ead9de2ca4d8ef01cdb34e64b755
Jeferson Vieira <jeferson.vieira.dev@gmail.com>
2026-07-03 18:05:07 -0300

    feat: initialize project with Docker Compose and multi-profile config

---
+++ src/main/java/com/jeferson/showpass/ShowpassApplication.java
@@ -0,0 +1,13 @@
+package com.jeferson.showpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowpassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowpassApplication.class, args);
+	}
+
+}
